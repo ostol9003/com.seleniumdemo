fbd532e25972799fb22042ff7d9ca11306ecf020
Ostolski Marcin <ostolski.marcin@gmail.com>
2023-04-10 18:09:29 +0200

    Add OrderDetailPage, add assertions

--- src/test/java/com/seleniumdemo/pages/AddressDetailsPage.java
+++ src/test/java/com/seleniumdemo/pages/AddressDetailsPage.java
@@ -1,13 +1,69 @@
 package com.seleniumdemo.pages;
 
+import com.seleniumdemo.models.Customer;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddressDetailsPage {
-    WebDriver driver;
+
+    @FindBy(id = "billing_first_name")
+    private WebElement firstNameInput;
+
+    @FindBy(id = "billing_last_name")
+    private WebElement lastNameInput;
+
+    @FindBy(id = "billing_company")
+    private WebElement companyNameInput;
+
+    @FindBy(id = "billing_country")
+    private WebElement billingCountrySelect;
+
+    @FindBy(id = "billing_address_1")
+    private WebElement billingAddresInput;
+
+    @FindBy(id = "billing_postcode")
+    private WebElement billingPostCodeInput;
+
+    @FindBy(id = "billing_city")
+    private WebElement billingCityInput;
+
+    @FindBy(id = "billing_phone")
+    private WebElement billingPhoneInput;
+
+    @FindBy(id = "billing_email")
+    private WebElement billingEmailInput;
+
+    @FindBy(id = "order_comments")
+    private WebElement orderCommentsInput;
+
+    @FindBy(id = "place_order")
+    private WebElement placeOrderButton;
+
+    private WebDriver driver;
 
     public AddressDetailsPage(WebDriver driver) {
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
         this.driver = driver;
+    }
+
+    public OrderDetailsPage fillAddress(Customer customer, String comments) {
+        firstNameInput.sendKeys(customer.getFirstName());
+        lastNameInput.sendKeys(customer.getLastName());
+        companyNameInput.sendKeys(customer.getCompanyName());
+        Select countrySelect = new Select(billingCountrySelect);
+        countrySelect.selectByVisibleText(customer.getCountry());
+        billingAddresInput.sendKeys(String.format("%s %s", customer.getStreet(), customer.getFlatNumber()));
+        billingPostCodeInput.sendKeys(customer.getZipCode());
+        billingCityInput.sendKeys(customer.getCity());
+        billingPhoneInput.sendKeys(customer.getPhone());
+        billingEmailInput.sendKeys(customer.getEmail());
+        orderCommentsInput.sendKeys(comments);
+        Actions act = new Actions(driver);
+        act.doubleClick(placeOrderButton).perform();
+        return new OrderDetailsPage(driver);
     }
 }
--- src/test/java/com/seleniumdemo/test/CheckoutTest.java
+++ src/test/java/com/seleniumdemo/test/CheckoutTest.java
@@ -1,17 +1,27 @@
 package com.seleniumdemo.test;
 
+import com.seleniumdemo.models.Customer;
 import com.seleniumdemo.pages.HomePage;
+import com.seleniumdemo.pages.OrderDetailsPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CheckoutTest extends BaseTest {
 
     @Test
     public void checkoutTest() {
-        new HomePage(driver)
+        Customer customer = new Customer();
+
+        OrderDetailsPage orderDetailsPage = new HomePage(driver)
                 .openShopPage()
                 .openProductPage("Java Selenium WebDriver")
                 .addProductToCart()
                 .viewCart()
-                .openAddressDetails();
+                .openAddressDetails()
+                .fillAddress(customer, "some comment");
+
+        Assert.assertEquals(orderDetailsPage.getOrderNotice().getText(),"Thank you. Your order has been received.");
+        Assert.assertEquals(orderDetailsPage.getProductName().getText(),"Java Selenium WebDriver × 1");
     }
+
 }
